8337fef5fc6db133d6f858ab0de0658900b7b143
Max Zurawski <maksymilian.zurawski@consileon.pl>
2019-10-15 23:50:13 +0200

    adjusting run.sh script

--- run.sh
+++ run.sh
@@ -5,6 +5,11 @@ echo "Waiting for the eurekadiscovery service to start on port 8761"
 echo "**************************************************************"
 while ! `nc -z discovery 8761 `; do sleep 3; done
 
+echo "#############################################"
+echo "Waiting for proxy"
+echo "#############################################"
+while ! `nc -z proxy 8000 `; do sleep 3; done
+
 echo "**************************************************************"
 echo "Waiting for the rabbit service to start "
 echo "**************************************************************"
